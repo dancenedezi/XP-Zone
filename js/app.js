ecb8a8a55a82a854d9fd407af01ea51ef6e1f9fb
Daniel Cenedezi <dancenedezi@gmail.com>
2025-01-14 12:32:47 -0300

    adding mobile modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const bxButton = document.querySelector(".bx");
+const modalMobile = document.querySelector(".modal-mobile");
 
 bxButton.addEventListener("click", () => {
   bxButton.classList.toggle("bx-active");
+  modalMobile.classList.toggle("modal-mobile-active");
 });
